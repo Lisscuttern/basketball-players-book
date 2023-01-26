503941549329e1a84f87b65d6e719ba1ee755032
Ozan <73613081+Lisscuttern@users.noreply.github.com>
2023-01-25 17:49:43 -0800

    ++ design

---
+++ BaskeballPlayersBook/BaskeballPlayersBook/DetailsVC.swift
@@ -0,0 +1,26 @@
+//
+//  DetailsVC.swift
+//  BaskeballPlayersBook
+//
+//  Created by Ozan İncedayı on 25.01.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController
+{
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var teamLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
---
+++ BaskeballPlayersBook/BaskeballPlayersBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaskeballPlayersBook
+//
+//  Created by Ozan İncedayı on 25.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+    }
+}
+
